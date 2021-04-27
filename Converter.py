3eeece9ecd880e8e9dda65698f28423b5c1252fc
james <james_d02@protonmail.com>
2021-04-27 19:18:12 +0100

    Updated markdown converter code & updated styling

--- Converter.py
+++ Converter.py
@@ -1,9 +1,16 @@
 import os
 import markdown 
 
+from sys import argv
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+
+from jinja2 import Environment, FileSystemLoader
+env = Environment(loader=FileSystemLoader('src/templates'))
+
 def getMarkdownFilesFromDirectory(directory): 
     files = []
-
     with os.scandir(directory) as dir:
         for entry in dir: 
             if(entry.is_dir()):
@@ -12,15 +19,41 @@ def getMarkdownFilesFromDirectory(directory):
                 files.append(entry)
     return files 
 
+def getFoldersFromDirectory(directory):
+    folders = []
+    with os.scandir(directory) as dir:
+        for entry in dir: 
+            if(entry.is_dir()):
+                folders.append(entry)
+    return folders 
 
-files = getMarkdownFilesFromDirectory("input")
+def getReferences(files):
+    references = []
+    for f in files:
+        filename = os.path.splitext(f.name)[0].capitalize()
+        references.append(filename)
+    return references
 
-
-for f in files:
-
+def writeToFile(f, references):
     filename = os.path.splitext(f.name)[0]
+    md = markdown.Markdown(extensions=['toc'])
     with open(f.path) as input: 
         text = input.read()
-        html = markdown.markdown(text)
-    with open("output/{file}.html".format(file=filename), 'w+') as output:
-        output.write(html)
+        html = md.convert(text)
+
+    template = env.get_template('page.html')
+    with open("output/{file}.html".format(file=filename), 'w+', encoding="utf-8") as output: 
+        template_data = template.render(html=html, references=references)
+        output.write(template_data)
+
+def main(folder):
+    files = getMarkdownFilesFromDirectory(folder)
+    references = getReferences(files)
+
+    for f in files:
+        print("Processing file: {file}".format(file=f.name))
+        writeToFile(f, references)
+
+if __name__ == "__main__":
+    folder = str(argv[1]) 
+    main(folder)
--- output/public/js/utility.js
+++ output/public/js/utility.js
@@ -2,21 +2,21 @@ const sidebar = document.getElementById("sidebar");
 const container = document.getElementById("container");
 const navbar = document.getElementById("navbar");
 
-if(localStorage.getItem('theme') != null){
+if(localStorage.getItem('markoveter-theme') != null){
+    console.log("Setting to stored theme...");
     setTheme(localStorage.getItem('markoveter-theme'))
 } else {
+    console.log("Setting to light theme...");
     setTheme('theme-light');
 }
 
 function toggleSidebar(){
-    if(sidebar.style.display === "none"){
+    if(sidebar.style.display === "none" || sidebar.style.display === ""){
+        console.log("Enabling sidebar...");
         sidebar.style.display = "flex";
-        container.classList.toggle("enlarged")
-        navbar.classList.toggle("enlarged");
     } else {
+        console.log("Disabling sidebar...");
         sidebar.style.display = "none";
-        container.classList.toggle("enlarged")
-        navbar.classList.toggle("enlarged");
     }
 }
 
